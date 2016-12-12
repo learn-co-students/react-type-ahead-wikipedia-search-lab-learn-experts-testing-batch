9d4ad4db72cf3ea796877d5d0ebd7af1c4925fb8
Enoch2k2 <enoch2k2@hotmail.com>
2016-12-12 11:52:37 -0600

    Done.

--- actions/index.js
+++ actions/index.js
@@ -8,7 +8,19 @@ const search = (query) => {
   const requested = new Date();
 
   return wikipedia.search(query).then((data) => {
-    // TODO
+    var newData = [];
+    if(data){
+      for (var i = 0; i < data.length - 2; i++) {
+        newData.push({
+          description: data[2][i],
+          link: data[3][i],
+          title: data[1][i]
+        });
+      }
+    }
+    if(!resultStore.isOutdated(requested)){
+      resultStore.setState({results: newData, updated: requested});
+    }
   });
 };
 
--- components/SearchField.js
+++ components/SearchField.js
@@ -3,7 +3,8 @@
 const React = require('react');
 
 const SearchField = props => (
-  <input />
+  <input className="search-field" value={props.value}
+    onChange={props.onChange}/>
 );
 
 module.exports = SearchField;
