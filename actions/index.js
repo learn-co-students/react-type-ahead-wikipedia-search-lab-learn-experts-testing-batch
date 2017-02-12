f635ed630aa5507dec8a53f679d902ec920099ca
pajamaw <pjpeterww@gmail.com>
2017-02-11 20:06:46 -0500

    Done.

--- actions/index.js
+++ actions/index.js
@@ -3,12 +3,39 @@
 const jsonp = require('jsonp');
 const resultStore = require('../stores/resultStore');
 const wikipedia = require('../utils/wikipedia');
-
+// const data = [
+//   'query',
+//   ['title 1', 'title 2'],
+//   ['description 1', 'description 2'],
+//   ['link 1', 'link 2']
+// ];
 const search = (query) => {
   const requested = new Date();
 
   return wikipedia.search(query).then((data) => {
-    // TODO
+      //const newData = data.slice(1, data.length)
+      if (resultStore.isOutdated(requested)){
+        return;
+      }
+      const [query, titles, descriptions, links] = data
+
+      const results = titles.map((title, i)=>({
+        title,
+        description: descriptions[i],
+        link: links[i]
+      }))
+      // const formattedData = [];
+      //   for(i=0; i<newData[0].length;i++){
+      //     formattedData.push({
+      //       title: newData[0][i],
+      //       description: newData[1][i],
+      //       link: newData[2][i]
+      //     })
+      //   }
+        resultStore.setState({
+          results,
+          updated: requested
+        })
   });
 };
 
